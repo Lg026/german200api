3809bea54980261f96f2ecb3d375d52fe723e52c
Lg026 <130603739+Lg026@users.noreply.github.com>
2023-09-27 15:17:51 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+
+const wordSchema = new mongoose.Schema({
+  German: String,
+  English: String,
+  Example: String,
+  EnglishExample: String
+});
+
+const Word = mongoose.model('Word', wordSchema);
+
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Database connected!')).catch(err => console.log(err));
+
+app.get('/words', (req, res) => {
+  Word.find({})
+    .then(words => res.json(words))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
