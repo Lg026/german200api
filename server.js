0a8d11142e09c5bad5001afd4f549a535779a41f
Lg026 <130603739+Lg026@users.noreply.github.com>
2023-10-03 20:03:01 -0700

    reset changes

--- server.js
+++ server.js
@@ -14,16 +14,13 @@ const wordSchema = new mongoose.Schema({
 
 const Word = mongoose.model('Word', wordSchema);
 
-mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 5 })
-  .then(() => console.log('Database connected!'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.DB_STRING, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('Database connected!')).catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-  Word.find({}, { _id: 0 })
+  Word.find({})
     .then(words => res.json(words))
     .catch(err => res.status(500).json({ error: err.message }));
 });
-
 console.log(Word.find({}))
 
 const port = process.env.PORT || 3000;
